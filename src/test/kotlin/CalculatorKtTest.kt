a3e6de90374e98e76d159afd4e67f64ea342d795
Puntawat Subhamani <puntawatsub@gmail.com>
2026-03-18 14:09:47 +0200

    fix

---
+++ src/test/kotlin/CalculatorKtTest.kt
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions.*
+
+class CalculatorKtTest {
+    @org.junit.jupiter.api.Test
+    fun addMe() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun subMe() {
+    }
+
+}
